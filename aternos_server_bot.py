a249c978b54c0e43bc8da56252739842246bad00
PROJECT-skd <mosestolsty@gmail.com>
2024-06-19 18:33:18 +0300

    aternos_server_bot.py

--- aternos_server_bot.py
+++ aternos_server_bot.py
@@ -3,11 +3,11 @@ import os
 from python_aternos import Client
 import time
 
-TOKEN = 'paste-your-token-here'
+TOKEN = 'MTI1Mjg5MTE0NjE5MTkwMDY3NA.GB52ka.3tpZwX8DajZGt29cF0UpAsDcR1UkZbdMVT00xQ'
 
 client = discord.Client()
 
-aternos = Client('your-aternos-username', password='your-aternos-password')
+aternos = Client('Moses3031', password='gyatgyatgyatgyat1234^*$$$$%^&&^^%$#@#@#$%ohiorizz')
 
 atservers = aternos.servers
 
@@ -37,12 +37,12 @@ async def on_message(message):
         if user_message.lower() == '?server_start':
           myserv.start()
           while True:
-            ping = str(os.popen('mcstatus yourservername.aternos.me status | grep description').read())
+            ping = str(os.popen('mcstatus darkplaysmp.aternos.me status | grep description').read())
             if "offline" in ping:
               time.sleep(1)
             else:
               break
-          await message.channel.send("server is now alive!!! you can join in 2-3 minutes by pasting ||yourservername.aternos.me:serverport|| in the server address.")
+          await message.channel.send("server is now alive!!! you can join in 2-3 minutes by pasting ||darkplaysmp.aternos.me:43102|| in the server address.")
           return
 
     if message.channel.name == 'bot-cmnds':
